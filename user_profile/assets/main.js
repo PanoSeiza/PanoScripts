547765f2a52af9acb6e949d41f601b6d49bb1d6c
JustRandom <justrandom.work@gmail.com>
2023-03-10 17:17:50 +0300

    Initial commit

---
+++ user_profile/assets/main.js
@@ -0,0 +1,54 @@
+    // Initialise Apps framework client. See also:
+    var client = ZAFClient.init()
+    client.invoke('resize', { width: '0px', height: '0px' })
+
+    
+    async function getUserRedeem() {
+        response = await client.get("ticket.customField:custom_field_360011578900")
+        redeem = response["ticket.customField:custom_field_360011578900"]
+        console.log('Got Redeem '+ redeem)    
+    return redeem
+    }
+  
+    async function constructUrl() {
+        response = await client.get("ticket.customField:custom_field_360011578900")
+        redeem = response["ticket.customField:custom_field_360011578900"]
+        console.log('Got Redeem '+ redeem)
+
+        response = await client.get("ticket.customField:custom_field_360015024660")
+        game = response["ticket.customField:custom_field_360015024660"]
+        console.log('Got Project ' + game)
+        //abc - Auto Brawl Chess
+        //bz  - Brawl Zone
+        //mp  - Mighty Party
+        //mw  - Mighty Wars
+        var adminUrl
+        switch (game){
+            case 'auto_brawl_chess':
+                adminUrl = 'https://dev1.panochess.com/__admin__/#/!/profile/search/redeem/' + redeem
+                break;
+            case 'brawl_zone':
+                adminUrl = 'https://dev1.panochess.com/__admin__/#/!/profile/search/redeem/' + redeem
+                break;
+            case 'mighty_party':
+                adminUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' //replace with url when frontend task is done
+                break;
+            case 'mighty_wars':
+                adminUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' //replace with url when frontend task is done
+                break;
+            default:
+                adminUrl = 'https://blitzmightyparty.com/__admin__/#/!/profile/search'
+        }
+
+
+    return adminUrl
+    }
+
+
+    client.on('pane.activated', async function() {
+        urlRedeem = await constructUrl()
+        console.log('Opening link')
+        window.open(urlRedeem, '_blank')
+
+    });
+
